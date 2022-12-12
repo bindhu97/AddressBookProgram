e130edb205cb42eea657a0ca133a94e10b78e8b1
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-12 23:06:35 +0530

    [Bindhu]Refactor to add Multiple Address Book To System

--- Program.cs
+++ Program.cs
@@ -14,8 +14,10 @@
                     "2) Find the Display of Contacts\n" +
                     "3) Find the edit contacts\n" +
                     "4) Find the delete contacts\n" + 
-                    "5) Find the ability to add multiple Person\n");
+                    "5) Find the ability to add multiple Person\n" + 
+                    "6) Add multiple address to system\n");
                 int option = Convert.ToInt32(Console.ReadLine());
+                int count = 0;
                 switch (option)
                 {
                     case 1:
@@ -36,6 +38,21 @@
                         break;
 
                     case 5:
+                        Person.AddContact();
+                        break;
+
+                    case 6:
+                        int addCount = Convert.ToInt32(Console.ReadLine());
+
+                        Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+                        for (int i = 1; i <= addCount; i++)
+                        {
+                            count++;
+                            List<Person> newAdd = new List<Person>();
+                            dictionary.Add(i, "New Dictionary");
+                        }
+                        Console.WriteLine(count + " Address Book Created..");
                         Person.AddContact();
                         break;
                     default:

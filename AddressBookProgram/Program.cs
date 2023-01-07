10c6d2cbc1faf085b48ae5365c45c531ad32952b
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-07 18:34:45 +0530

    [Bindhu]Add:Ability to ensure no Duplicate entry

--- AddressBookProgram/Program.cs
+++ AddressBookProgram/Program.cs
@@ -22,7 +22,8 @@ namespace AddressBookProgram
                     "4) Find the delete contacts\n" +
                    "5) Ability to add Multiple person in AddressBook\n" +
                     "6) Ability to add Multiple AddressBook using Dictionary\n" +
-                    "7) Find the DisplayList of Dictionary\n");
+                    "7) Find the DisplayList of Dictionary\n" +
+                    "8) Ability to ensure there is no Duplicate entry of same person\n");
 
                 int option = Convert.ToInt32(Console.ReadLine());
                 int count = 0;
@@ -57,6 +58,11 @@ namespace AddressBookProgram
                     case 7:
                         Person.DisplayList();
                         break;
+
+                    case 8:
+                        Person.DuplicateContact();
+                        break;
+
 
                     default:
                         Console.WriteLine("Choose valid option");

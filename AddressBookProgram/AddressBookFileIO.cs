c81e70d8f7817282c9e8f5e051c7995139f969d8
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-07 23:55:23 +0530

    [Bindhu]Add: Ability to read And ReadAllLines in FileIO

---
+++ AddressBookProgram/AddressBookFileIO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    public class AddressBookFileIO
+    {
+        public static void FileExists()
+        {
+            String path = @"C:\Users\91973\source\repos\AddressBookProgram\AddressBookProgram\AddressBook.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            Console.ReadKey();
+        }
+        public static void ReadAllLines()
+        {
+            String path = @"C:\Users\91973\source\repos\AddressBookProgram\AddressBookProgram\AddressBook.txt";
+            String[] lines;
+            lines = File.ReadAllLines(path);
+
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+            Console.WriteLine(lines[2]);
+            Console.WriteLine(lines[3]);
+            Console.WriteLine(lines[4]);
+            Console.WriteLine(lines[5]);
+            Console.WriteLine(lines[6]);
+
+            Console.ReadKey();
+        }
+    }
+}
